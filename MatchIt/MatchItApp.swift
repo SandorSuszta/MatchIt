0c7bd3f0d82150126cc6a320b6cb2fe277a91eff
Sandor Suszta <sasha.shusta@gmail.com>
2023-06-08 16:35:49 +0100

    Initial Commit

---
+++ MatchIt/MatchItApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchItApp.swift
+//  MatchIt
+//
+//  Created by Nataliia Shusta on 08/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct MatchItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
